8d134d2ce9c6712e5ff7b3ec5b3e9a123ab69a80
Paweł Borowski <p.borowski88@gmail.com>
2016-10-14 21:21:08 +0200

    removed map-editor reducer

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { router } from 'redux-ui-router';
-import mapEditor from './map-editor';
 import currentEnrichedMap from './current-enriched-map';
 
 const reducers = combineReducers({
   router,
   currentEnrichedMap,
-  mapEditor
 });
 
 export default reducers;
