bf2fc022e539c8fcd4e12c7f34d7a6875df045e3
Paweł Borowski <p.borowski88@gmail.com>
2016-10-15 19:06:19 +0200

    Added karma for testing

---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function(config) {
+  config.set({
+    browsers: ['PhantomJS'],
+    files: [{
+      pattern: 'test-context.js',
+      watched: false
+    }],
+    frameworks: ['jasmine'],
+    preprocessors: {
+      'test-context.js': ['webpack']
+    },
+    webpack: {
+      module: {
+        loaders: [{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: ['ng-annotate', 'babel']
+        }]
+      },
+      watch: true
+    },
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
